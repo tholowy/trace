6c4c02ea8411b2292c450d59173cb9816a5bdc1b
d3ter <tholowy@gamil.com>
2025-05-21 21:12:02 -0500

    [fix] remove basename

--- assets/classDiagram-v2-COTLJTTW-BVNGyMYb.js
+++
@@ -1,1 +0,0 @@
-import{s as r,c as s,a as e,C as t}from"./chunk-A2AXSNBT-BFaBB3_a.js";import{_ as l}from"./index-Cd-eXTn4.js";import"./chunk-RZ5BOZE2-DeKVAuW-.js";var m={parser:e,get db(){return new t},renderer:s,styles:r,init:l(a=>{a.class||(a.class={}),a.class.arrowMarkerAbsolute=a.arrowMarkerAbsolute},"init")};export{m as diagram};
