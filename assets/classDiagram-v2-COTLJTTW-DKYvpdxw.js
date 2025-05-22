b7f7d789100c2404e4d66c90318086af3ca608c5
d3ter <tholowy@gamil.com>
2025-05-21 20:20:31 -0500

    Add initial HTML structure and SVG icon for the project

---
+++ assets/classDiagram-v2-COTLJTTW-DKYvpdxw.js
@@ -0,0 +1,1 @@
+import{s as r,c as s,a as e,C as t}from"./chunk-A2AXSNBT-CFIUW0Al.js";import{_ as l}from"./index-BU88sJCT.js";import"./chunk-RZ5BOZE2-B5tql9u7.js";var m={parser:e,get db(){return new t},renderer:s,styles:r,init:l(a=>{a.class||(a.class={}),a.class.arrowMarkerAbsolute=a.arrowMarkerAbsolute},"init")};export{m as diagram};
---
+++ assets/clone-BV6fWqJ7.js
@@ -0,0 +1,1 @@
+import{b as r}from"./_baseUniq-2P1l1vYz.js";var e=4;function a(o){return r(o,e)}export{a as c};
