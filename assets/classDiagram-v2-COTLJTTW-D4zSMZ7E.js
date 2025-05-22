ab59bb34d8429fc7c31a2fc202141ed67f004076
d3ter <tholowy@gamil.com>
2025-05-21 20:48:44 -0500

    [fix] uso de hash router y base url

---
+++ assets/classDiagram-v2-COTLJTTW-D4zSMZ7E.js
@@ -0,0 +1,1 @@
+import{s as r,c as s,a as e,C as t}from"./chunk-A2AXSNBT-BzGBZ6A9.js";import{_ as l}from"./index-D9WbG2ZO.js";import"./chunk-RZ5BOZE2-7O0hYj6E.js";var m={parser:e,get db(){return new t},renderer:s,styles:r,init:l(a=>{a.class||(a.class={}),a.class.arrowMarkerAbsolute=a.arrowMarkerAbsolute},"init")};export{m as diagram};
--- assets/classDiagram-v2-COTLJTTW-ivI9muiX.js
+++
@@ -1,1 +0,0 @@
-import{s as r,c as s,a as e,C as t}from"./chunk-A2AXSNBT-CrEywbPe.js";import{_ as l}from"./index-CcZclqrR.js";import"./chunk-RZ5BOZE2-B8aFpEOE.js";var m={parser:e,get db(){return new t},renderer:s,styles:r,init:l(a=>{a.class||(a.class={}),a.class.arrowMarkerAbsolute=a.arrowMarkerAbsolute},"init")};export{m as diagram};
